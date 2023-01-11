82d71ed7b758d80d8d08a62a8a3f4126d850e380
Dustin Shunta <dustinhans@gmail.com>
2023-01-11 05:12:43 -0500

    trying to commit

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -2,7 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-//checking if i can commit
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
